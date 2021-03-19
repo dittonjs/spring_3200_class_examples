339ddc0c4cba62e3eb9e93ad44dc4179418bd71b
Joseph Ditton <jditton.atomic@gmail.com>
2021-03-19 14:23:39 -0600

    class today

---
+++ MaterialStuff/app/src/main/java/com/usu/materialstuff/HomeFragment.java
@@ -0,0 +1,15 @@
+package com.usu.materialstuff;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class HomeFragment extends Fragment {
+    public HomeFragment() {
+        super(R.layout.fragment_home);
+    }
+}
--- MaterialStuff/app/src/main/java/com/usu/materialstuff/MainActivity.java
+++ MaterialStuff/app/src/main/java/com/usu/materialstuff/MainActivity.java
@@ -1,8 +1,11 @@
 package com.usu.materialstuff;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+
+import com.google.android.material.appbar.MaterialToolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MaterialToolbar toolbar = findViewById(R.id.topAppBar);
+        toolbar.setNavigationOnClickListener(view -> {
+            DrawerLayout layout = findViewById(R.id.drawer_layout);
+            layout.open();
+        });
     }
 }
