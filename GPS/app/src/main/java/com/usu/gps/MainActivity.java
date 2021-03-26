1bc713be3fa0ea49a34365fe101514280d4509cd
Joseph Ditton <jditton.atomic@gmail.com>
2021-03-26 14:24:30 -0600

    adds maps

--- GPS/app/src/main/java/com/usu/gps/MainActivity.java
+++ GPS/app/src/main/java/com/usu/gps/MainActivity.java
@@ -12,28 +12,113 @@ import android.location.LocationManager;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.maps.Style;
+import com.mapbox.mapboxsdk.plugins.markerview.MarkerView;
+import com.mapbox.mapboxsdk.plugins.markerview.MarkerViewManager;
 
+public class MainActivity extends AppCompatActivity {
+    MapView mapView;
+    MarkerView userMarker;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Mapbox.getInstance(this, getString(R.string.mapbox_access_token));
         setContentView(R.layout.activity_main);
-        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-        }
-        TextView locationTextView = findViewById(R.id.location);
-        LocationManager manager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
-        manager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1, new LocationListener() {
+
+        mapView = findViewById(R.id.map);
+        mapView.onCreate(savedInstanceState);
+
+
+
+
+
+        mapView.getMapAsync(new OnMapReadyCallback() {
             @Override
-            public void onLocationChanged(@NonNull Location location) {
-                locationTextView.setText(String.format("Your initial location is %f by %f\n", location.getLatitude(), location.getLongitude()));
+            public void onMapReady(@NonNull MapboxMap mapboxMap) {
+                mapboxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
+                    @Override
+                    public void onStyleLoaded(@NonNull Style style) {
+                        LocationManager manager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+                        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                            requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+                        }
+                        MarkerViewManager markerViewManager = new MarkerViewManager(mapView, mapboxMap);
+
+
+                        manager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1, new LocationListener() {
+                            @Override
+                            public void onLocationChanged(@NonNull Location location) {
+                                if (userMarker == null) {
+                                    TextView textView = new TextView(MainActivity.this);
+                                    textView.setText("Hello, I am here!");
+                                    userMarker = new MarkerView(new LatLng(location.getLatitude(), location.getLongitude()), textView);
+                                    markerViewManager.addMarker(userMarker);
+                                } else {
+                                    userMarker.setLatLng(new LatLng(location.getLatitude(), location.getLongitude()));
+                                }
+                            }
+                        });
+                    }
+                });
             }
         });
+
+
+
+
+//
     }
 
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
     }
 }
