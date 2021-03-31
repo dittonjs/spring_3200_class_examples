3ae9d3beb7af2857129e67c93ec675ba798d3418
Joseph Ditton <jditton.atomic@gmail.com>
2021-03-31 13:23:33 -0600

    fixes gps

--- GPS/app/src/main/java/com/usu/gps/MainActivity.java
+++ GPS/app/src/main/java/com/usu/gps/MainActivity.java
@@ -6,24 +6,42 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.mapbox.geojson.Feature;
+import com.mapbox.geojson.FeatureCollection;
+import com.mapbox.geojson.LineString;
+import com.mapbox.geojson.Point;
 import com.mapbox.mapboxsdk.Mapbox;
 import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.location.LocationComponent;
+import com.mapbox.mapboxsdk.location.LocationComponentActivationOptions;
+import com.mapbox.mapboxsdk.location.LocationComponentOptions;
+import com.mapbox.mapboxsdk.location.OnCameraTrackingChangedListener;
+import com.mapbox.mapboxsdk.location.OnLocationCameraTransitionListener;
+import com.mapbox.mapboxsdk.location.modes.CameraMode;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 import com.mapbox.mapboxsdk.maps.Style;
 import com.mapbox.mapboxsdk.plugins.markerview.MarkerView;
 import com.mapbox.mapboxsdk.plugins.markerview.MarkerViewManager;
+import com.mapbox.mapboxsdk.style.layers.LineLayer;
+import com.mapbox.mapboxsdk.style.layers.Property;
+import com.mapbox.mapboxsdk.style.layers.PropertyFactory;
+import com.mapbox.mapboxsdk.style.sources.GeoJsonSource;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     MapView mapView;
     MarkerView userMarker;
+    ArrayList<Point> points = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,20 +65,111 @@ public class MainActivity extends AppCompatActivity {
                         if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
                             requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 1);
                         }
+                        GeoJsonSource source = new GeoJsonSource("points",
+                                FeatureCollection.fromFeatures(new Feature[]{
+                                        Feature.fromGeometry(
+                                                LineString.fromLngLats(points)
+                                        )
+                                })
+                        );
+
+                        style.addSource(source);
+
+                        style.addLayer(new LineLayer("line-layer", "points").withProperties(
+                                PropertyFactory.lineDasharray(new Float[] {0.01f, 2f}),
+                                PropertyFactory.lineCap(Property.LINE_CAP_ROUND),
+                                PropertyFactory.lineJoin(Property.LINE_JOIN_ROUND),
+                                PropertyFactory.lineWidth(5f),
+                                PropertyFactory.lineColor(Color.parseColor("#e55e5e"))
+                        ));
+
+
                         MarkerViewManager markerViewManager = new MarkerViewManager(mapView, mapboxMap);
+                        mapboxMap.addOnMapClickListener(new MapboxMap.OnMapClickListener() {
+                            @Override
+                            public boolean onMapClick(@NonNull LatLng point) {
+
+                                return true;
+                            }
+                        });
+
+                        mapboxMap.addOnMapLongClickListener(new MapboxMap.OnMapLongClickListener() {
+                            @Override
+                            public boolean onMapLongClick(@NonNull LatLng point) {
+
+
+
+
+                                return false;
+                            }
+                        });
+
+                        LocationComponent locationComponent = mapboxMap.getLocationComponent();
+
+                        locationComponent.activateLocationComponent(
+                                LocationComponentActivationOptions.builder(MainActivity.this, style)
+                                        .useSpecializedLocationLayer(true)
+                                        .build()
+                        );
+
+                        locationComponent.setLocationComponentEnabled(true);
+
+                        locationComponent.setCameraMode(CameraMode.TRACKING_GPS_NORTH, new OnLocationCameraTransitionListener() {
+                            @Override
+                            public void onLocationCameraTransitionFinished(int cameraMode) {
+                                locationComponent.zoomWhileTracking(17, 100);
+                            }
+
+                            @Override
+                            public void onLocationCameraTransitionCanceled(int cameraMode) {
+
+                            }
+                        });
+
+
+                        LocationComponentOptions options = locationComponent.getLocationComponentOptions().toBuilder()
+                                .trackingGesturesManagement(true)
+                                .trackingInitialMoveThreshold(500)
+                                .build();
+
+                        locationComponent.applyStyle(options);
+
+
+
+                        locationComponent.addOnCameraTrackingChangedListener(new OnCameraTrackingChangedListener() {
+                            @Override
+                            public void onCameraTrackingDismissed() {
+                                System.out.println("DONE TRACKING");
+                                new Thread(() -> {
+                                    try {
+                                        Thread.sleep(5000);
+                                    } catch (InterruptedException e) {
+                                        e.printStackTrace();
+                                    }
+                                    runOnUiThread(() -> {
+                                        locationComponent.setCameraMode(CameraMode.TRACKING_GPS_NORTH);
+                                    });
+                                }).start();
+                            }
+
+                            @Override
+                            public void onCameraTrackingChanged(int currentMode) {
+                                System.out.println("TRACKING CHANGED " + currentMode);
+                            }
+                        });
+
+
 
 
                         manager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1, new LocationListener() {
                             @Override
                             public void onLocationChanged(@NonNull Location location) {
-                                if (userMarker == null) {
-                                    TextView textView = new TextView(MainActivity.this);
-                                    textView.setText("Hello, I am here!");
-                                    userMarker = new MarkerView(new LatLng(location.getLatitude(), location.getLongitude()), textView);
-                                    markerViewManager.addMarker(userMarker);
-                                } else {
-                                    userMarker.setLatLng(new LatLng(location.getLatitude(), location.getLongitude()));
-                                }
+                                points.add(Point.fromLngLat(location.getLongitude(), location.getLatitude()));
+                                source.setGeoJson(FeatureCollection.fromFeatures(new Feature[]{
+                                        Feature.fromGeometry(
+                                                LineString.fromLngLats(points)
+                                        )
+                                }));
                             }
                         });
                     }
